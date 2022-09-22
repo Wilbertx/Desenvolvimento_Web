8043efe0a968ac7632ba65bf9918c2a1b2b99e3d
Matheus Wilbert <matheuswilbert2012@gmail.com>
2022-09-22 19:58:23 -0300

    Adicionado contato e postcss.config

---
+++ contato.php
@@ -0,0 +1,32 @@
+<?php
+include "header.php";
+?>
+
+<div class="container">
+
+
+</div>
+
+<?php
+include "footer.php";
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1>Fale conosco</h1>
+            <div class="">
+                <form action="">
+                    <label for="nome"></label>
+                    <input type="text" name="nome" id="nome">
+                    <input for="email">Digite seu e-mail</label>
+                    <input type="text" name="email" id="email">
+                    <input type="submit" value="Enviar">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+include "footer.php";
+?>

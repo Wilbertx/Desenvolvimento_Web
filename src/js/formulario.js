73772c6531d4b05a0ec775f738f801bc4e70be95
Matheus Wilbert <matheuswilbert2012@gmail.com>
2022-09-23 19:27:03 -0300

    Commit formulario

--- src/js/formulario.js
+++ src/js/formulario.js
@@ -1,18 +1,32 @@
-//const form = document.getElementById("formCadastro");
-//console.log.apply(form);
-
+// criamos um função para iniciar o nosso modulo
 const initForm = (qualForm) => {
-    const formRequerido = document.getElementById(qualForm);
-    const inputs = formRequerido.querySelectorAll("input");
-
+    // buscamos os filhos dele (inputs)
+    const inputs = qualForm.querySelectorAll("input");
     // perguntamos se existe algum resultado
-    if (inputs.length > 0) {
-        // se existir, percorremos o array de inputs    
+    if (inputs) {
+        // se existir, percorremos o array de inputs
         inputs.forEach((input) => {
             // exibir no console o valor de cada input
             console.log(input.value);
         });
+    } else {
+        // se não existir, exibimos uma mensagem no console
+        console.log("Não existe inputs neste formulário");
     }
 }
 
-export default form;
+const init = (qualForm) => {
+    // recebe o elemento DOM de acordo com a String do ID
+    const formRequerido = document.getElementById(qualForm);
+    // busca o botão de submit para este formulário
+    const submit = formRequerido.querySelector("input[type=submit]");
+    // adiciona um evento de click no botão
+    submit.addEventListener("click", (evento) => {
+        // previne que o elemento, no caso o input submit, faça o que ele faz por padrão
+        evento.preventDefault();
+        // chama a função que criamos
+        initForm(formRequerido);
+    })
+}
+
+export default init;
